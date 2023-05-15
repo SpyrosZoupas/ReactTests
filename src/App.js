b197a31d0de0cea675b69e559c726b8175beb42f
SpyrosZoupas <103274476+SpyrosZoupas@users.noreply.github.com>
2023-05-15 20:01:12 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import HighRes from './HighRes.jpg';
 import './App.css';
 
 function App() {
+  const [loadTime, setLoadTime] = useState(0);
+  const [memoryUsage, setMemoryUsage] = useState(0);
+
+  useEffect(() => {
+    const startTime = performance.now();
+    window.addEventListener('load', () => {
+      const endTime = performance.now();
+      setLoadTime(endTime - startTime);
+    });
+
+    setMemoryUsage((performance.memory.usedJSHeapSize) / (1024 * 1024));
+  }, []);
+
+  const images = Array.from({ length: 2000 }, (_, i) => (
+    <img src={require('./HighRes.jpg')} alt="High Resolution" />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+       <header>
+          <ul class="border-bottom">
+            <li>
+              <h1>Benchmark Panel</h1>
+            </li>
+            <li>
+              <a href="https://localhost:7010">Server-side Tests</a>
+            </li>
+            <li>
+              <a href="https://localhost:7010/Home/Asp">ASP.NET</a>
+            </li>
+            <li>
+              <a href="http://localhost:4200">Angular</a>
+            </li>
+            <li>
+              <a href="http://localhost:3001">React</a>
+            </li>
+          </ul>
+    </header>
+    <div class="load-time">
+      Load time: {loadTime} ms
+      Memory usage: {memoryUsage} MB
     </div>
+    <div class="container">
+    <div className="grid-container">{images}</div>
+    </div>
+    </body>
   );
 }
 
